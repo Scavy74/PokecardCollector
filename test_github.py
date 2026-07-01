a32bf1cfc965d9912a94da89f402b633af0c169f
Scavy74 <56686576+Scavy74@users.noreply.github.com>
2026-07-01 18:42:53 +0200

    Update test_github.py

--- test_github.py
+++ test_github.py
@@ -1,1 +1,18 @@
+from core.github_manager import GitHubManager
 
+
+def main():
+
+    github = GitHubManager()
+
+    print("Connexion à GitHub...")
+
+    manifest = github.get_manifest()
+
+    print("\nManifest reçu :\n")
+
+    print(manifest)
+
+
+if __name__ == "__main__":
+    main()
